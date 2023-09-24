44d64ec95429bfcb414907a3e95ce16a37cf1c69
rzerpo <robertolopez@gmail.com>
2023-09-24 16:31:11 +0200

    Terminado el 7. Operable.

--- Tema-1/genericos/Operable/src/main/java/org/example/Operable.java
+++ Tema-1/genericos/Operable/src/main/java/org/example/Operable.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public interface Operable {
+public interface Operable<E> {
+
+    E suma(E o);
+
+    E resta(E o);
+
+    E multiplicacion(E o);
+
+    E division(E o);
 }
---
+++ Tema-1/genericos/Operable/src/test/java/org/example/OperableTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OperableTest {
+
+    Operable<Integer> calculadora = new Operable<Integer>() {
+
+        private Integer valor = 5;
+
+        @Override
+        public Integer suma(Integer o) {
+            return this.valor + o;
+        }
+
+        @Override
+        public Integer resta(Integer o) {
+            return this.valor - o;
+        }
+
+        @Override
+        public Integer multiplicacion(Integer o) {
+            return this.valor * o;
+        }
+
+        @Override
+        public Integer division(Integer o) {
+            return this.valor / o;
+        }
+    };
+
+    @Test
+    @DisplayName("sumaTest")
+    void sumaTest() {
+        assertEquals(10, calculadora.suma(5));
+        assertNotEquals(5, calculadora.suma(5));
+    }
+
+    @Test
+    @DisplayName("restaTest")
+    void restaTest() {
+        assertEquals(0, calculadora.resta(5));
+        assertNotEquals(5, calculadora.resta(5));
+    }
+
+    @Test
+    @DisplayName("multiplicacionTest")
+    void multiplicacionTest() {
+        assertEquals(25, calculadora.multiplicacion(5));
+        assertNotEquals(10, calculadora.multiplicacion(5));
+    }
+
+    @Test
+    @DisplayName("divisionTest")
+    void divisionTest() {
+        assertEquals(1, calculadora.division(5));
+        assertNotEquals(10, calculadora.division(5));
+    }
+}
