b277847a8fc9559fa776eacd3c405be8b30af961
RLScript <robertolopez@gmail.com>
2023-10-07 14:34:18 +0200

    add LambdaAndMethodReference folder

---
+++ Tema-1/LambdaAndMethodReference/playGround/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH));
+
+        // Prueba aquí tus streams:
+        List<String> lowCaloricDishesName = menu
+                .stream()
+                .filter(d -> d.getCalories() < 400)
+                .sorted(comparing(Dish::getCalories))
+                .map(Dish::getName)
+                .collect(toList());
+
+        // Visualiza el resultado aquí
+        System.out.println(lowCaloricDishesName);
+
+    }
+
+}
