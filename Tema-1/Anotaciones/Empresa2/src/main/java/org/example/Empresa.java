1567b3fb7d515e549c8b60c3a2e4884889ca04e0
rzerpo <robertolopezt75@gmail.com>
2023-10-16 13:30:24 +0200

    commit exament1

--- Tema-1/Anotaciones/Empresa2/src/main/java/org/example/Empresa.java
+++ Tema-1/Anotaciones/Empresa2/src/main/java/org/example/Empresa.java
@@ -14,62 +14,62 @@ import org.example.annotations.aTecnico;
 @aOficial(value = @aEmpleado(dni = "3", nombre = "3", apellidos = "3"), codigoTaller = 3, categoria = "3")
 public class Empresa {
 
-        private String nombre;
-        private List<org.example.Empleado> empleados;
+    private String nombre;
+    private List<org.example.Empleado> empleados;
 
-        public Empresa(String nombre) {
-                this.empleados = new ArrayList<>();
-                this.nombre = nombre;
-        }
+    public Empresa(String nombre) {
+        this.empleados = new ArrayList<>();
+        this.nombre = nombre;
+    }
 
-        public String getNombre() {
-                return nombre;
-        }
+    public String getNombre() {
+        return nombre;
+    }
 
-        public void setNombre(String nombre) {
-                this.nombre = nombre;
-        }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-        public <T extends Empleado> boolean add(T e) {
-                return empleados.add(e);
-        }
+    public <T extends Empleado> boolean add(T e) {
+        return empleados.add(e);
+    }
 
-        public static Empresa cargadorDeContexto(String nombre) {
-                Empresa empresa = new Empresa(nombre);
+    public static Empresa cargadorDeContexto(String nombre) {
+        Empresa empresa = new Empresa(nombre);
 
-                cargarDirectivos(empresa);
-                cargarTecnicos(empresa);
-                cargarOficiales(empresa);
+        cargarDirectivos(empresa);
+        cargarTecnicos(empresa);
+        cargarOficiales(empresa);
 
-                return empresa;
-        }
+        return empresa;
+    }
 
-        private static void cargarOficiales(Empresa empresa) {
-                List<aOficial> oficiales = Utils.recuperaAnotaciones(aOficial.class);
-                oficiales.stream()
-                                .map(Utils::crearOficial)
-                                .forEach(empresa::add);
-        }
+    private static void cargarOficiales(Empresa empresa) {
+        List<aOficial> oficiales = Utils.recuperaAnotaciones(aOficial.class);
+        oficiales.stream()
+                .map(Utils::crearOficial)
+                .forEach(empresa::add);
+    }
 
-        private static void cargarTecnicos(Empresa empresa) {
-                List<aTecnico> tecnicos = Utils.recuperaAnotaciones(aTecnico.class);
-                tecnicos.stream()
-                                .map(Utils::crearTecnico)
-                                .forEach(empresa::add);
-        }
+    private static void cargarTecnicos(Empresa empresa) {
+        List<aTecnico> tecnicos = Utils.recuperaAnotaciones(aTecnico.class);
+        tecnicos.stream()
+                .map(Utils::crearTecnico)
+                .forEach(empresa::add);
+    }
 
-        private static void cargarDirectivos(Empresa empresa) {
-                List<aDirectivo> directivos = Utils.recuperaAnotaciones(aDirectivo.class);
-                directivos.stream()
-                                .map(Utils::crearDirectivo)
-                                .forEach(empresa::add);
-        }
+    private static void cargarDirectivos(Empresa empresa) {
+        List<aDirectivo> directivos = Utils.recuperaAnotaciones(aDirectivo.class);
+        directivos.stream()
+                .map(Utils::crearDirectivo)
+                .forEach(empresa::add);
+    }
 
-        @Override
-        public String toString() {
-                return empleados.stream()
-                                .map(Empleado::toString)
-                                .collect(Collectors.joining("\n", "",
-                                                empleados.isEmpty() ? "Vacio" : ""));
-        }
+    @Override
+    public String toString() {
+        return empleados.stream()
+                .map(Empleado::toString)
+                .collect(Collectors.joining("\n", "",
+                        empleados.isEmpty() ? "Vacio" : ""));
+    }
 }
